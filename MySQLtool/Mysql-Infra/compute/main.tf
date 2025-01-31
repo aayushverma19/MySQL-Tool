fe05603fee57b82c996ed1b042071fa868cc0ae5
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-01-31 13:42:44 +0530

    main.tf

--- MySQLtool/Mysql-Infra/compute/main.tf
+++ MySQLtool/Mysql-Infra/compute/main.tf
@@ -57,64 +57,64 @@ resource "aws_instance" "private-instance2" {
 ####            compute module (target group)
 #######################################################################
 
-resource "aws_lb_target_group" "target" {
-  name     = var.tg_name
-  port     = var.tg_port
-  protocol = var.tg_protocol
-  vpc_id   = var.VPC_ID
-  health_check {
-    path                = var.health_check_path
-    interval            = var.health_check_interval
-    timeout             = var.health_check_timeout
-    healthy_threshold   = var.health_check_threshold
-    unhealthy_threshold = var.unhealth_check_threshold
-    matcher             = var.health_check_matcher
-  }
-}
+# resource "aws_lb_target_group" "target" {
+#   name     = var.tg_name
+#   port     = var.tg_port
+#   protocol = var.tg_protocol
+#   vpc_id   = var.VPC_ID
+#   health_check {
+#     path                = var.health_check_path
+#     interval            = var.health_check_interval
+#     timeout             = var.health_check_timeout
+#     healthy_threshold   = var.health_check_threshold
+#     unhealthy_threshold = var.unhealth_check_threshold
+#     matcher             = var.health_check_matcher
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "tg_attachment_private_instance1" {
-  target_group_arn = aws_lb_target_group.target.arn
-  target_id        = aws_instance.private-instance1.id
-  port             = var.tg_attachment_port
-}
+# resource "aws_lb_target_group_attachment" "tg_attachment_private_instance1" {
+#   target_group_arn = aws_lb_target_group.target.arn
+#   target_id        = aws_instance.private-instance1.id
+#   port             = var.tg_attachment_port
+# }
 
-resource "aws_lb_target_group_attachment" "tg_attachment_private_instance2" {
-  target_group_arn = aws_lb_target_group.target.arn
-  target_id        = aws_instance.private-instance2.id
-  port             = var.tg_attachment_port
-}
+# resource "aws_lb_target_group_attachment" "tg_attachment_private_instance2" {
+#   target_group_arn = aws_lb_target_group.target.arn
+#   target_id        = aws_instance.private-instance2.id
+#   port             = var.tg_attachment_port
+# }
 
-# ########################################################################################
-# ## Load Balancer
-# #######################################################################################
+# # ########################################################################################
+# # ## Load Balancer
+# # #######################################################################################
 
-resource "aws_lb" "MySQL-alb" {
-  name               = var.lb_name
-  internal           = var.lb_internal
-  load_balancer_type = var.lb_tpye
-  security_groups = [
-    var.pvt_sg 
-  ]
-  subnets = [
-    var.pub_sub_id, 
-    var.pvt_sub2_id 
-  ]
+# resource "aws_lb" "MySQL-alb" {
+#   name               = var.lb_name
+#   internal           = var.lb_internal
+#   load_balancer_type = var.lb_tpye
+#   security_groups = [
+#     var.pvt_sg 
+#   ]
+#   subnets = [
+#     var.pub_sub_id, 
+#     var.pvt_sub2_id 
+#   ]
 
-  enable_deletion_protection = var.enable_deletion
-}
+#   enable_deletion_protection = var.enable_deletion
+# }
 
 
-# ##################################################################################
-# ## Load Balancer listner
-# ##################################################################################
+# # ##################################################################################
+# # ## Load Balancer listner
+# # ##################################################################################
 
-resource "aws_lb_listener" "mysql_alb_listener" {
-  load_balancer_arn = aws_lb.MySQL-alb.arn
-  port              = var.alb_listener_port
-  protocol          = var.alb_listener_protocol
+# resource "aws_lb_listener" "mysql_alb_listener" {
+#   load_balancer_arn = aws_lb.MySQL-alb.arn
+#   port              = var.alb_listener_port
+#   protocol          = var.alb_listener_protocol
 
-  default_action {
-    type             = var.alb_listener_action
-    target_group_arn = aws_lb_target_group.target.arn
-  }
-}
+#   default_action {
+#     type             = var.alb_listener_action
+#     target_group_arn = aws_lb_target_group.target.arn
+#   }
+# }
